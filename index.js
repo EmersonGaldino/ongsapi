306edbef7194e62c2884a0b8fa356adb2404f806
Galdino <emersongaldino@hotmail.com>
2020-03-27 22:32:06 -0300

    incluindo nodemon

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
 const app = express();
-
+app.use(express.json());
 app.get('/users', (req, response) => {
   return response.json({
-    Users: [{ name: 'Galdino' }, { name: 'Ricardo' }, { name: 'Stella' }]
+    Users: [
+      { name: 'Galdino', stacks: [{ stack: '.Net' }, { stack: 'ReacJs' }] },
+      {
+        name: 'Ricardo',
+        stacks: [{ stack: 'ReactNative' }, { stack: 'Angular' }]
+      },
+      { name: 'Stella', stakcs: [{ stack: 'SQL' }, { stack: 'JS' }] }
+    ]
   });
 });
 
